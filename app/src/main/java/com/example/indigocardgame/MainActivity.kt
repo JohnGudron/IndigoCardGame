dc78090b9f2cd1f90edf232a7790404266888ff7
JohnGudron <oak43556@gmail.com>
2022-10-20 22:20:49 +0300

    Enable ViewBinding in buildGraddle(module), setContentView via binding. Make History, Rules, NewGame activities start when buttons on mainActivity clicked.

--- app/src/main/java/com/example/indigocardgame/MainActivity.kt
+++ app/src/main/java/com/example/indigocardgame/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.example.indigocardgame
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import com.example.indigocardgame.databinding.ActivityMainBinding
+import com.example.indigocardgame.databinding.ActivityNewGameBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        binding.newGameButton.setOnClickListener {
+            val intent:Intent = Intent(this, NewGame::class.java)
+            startActivity(intent)
+        }
+        binding.rulesButton.setOnClickListener {
+            val intent:Intent = Intent(this, Rules::class.java)
+            startActivity(intent)
+        }
+        binding.historyButton.setOnClickListener {
+            val intent:Intent = Intent(this, History::class.java)
+            startActivity(intent)
+        }
+    //startNewGame(binding.newGameButton)
+
     }
+
+    /*public fun startNewGame(view: View) {
+        val intent:Intent = Intent(this, ActivityNewGameBinding::class.java)
+        startActivity(intent)
+    }*/
 }
--- app/src/main/java/com/example/indigocardgame/NewGame.kt
+++ app/src/main/java/com/example/indigocardgame/NewGame.kt
@@ -2,10 +2,15 @@ package com.example.indigocardgame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.indigocardgame.databinding.ActivityMainBinding
+import com.example.indigocardgame.databinding.ActivityNewGameBinding
 
 class NewGame : AppCompatActivity() {
+    private lateinit var binding: ActivityNewGameBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_new_game)
+        binding = ActivityNewGameBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
